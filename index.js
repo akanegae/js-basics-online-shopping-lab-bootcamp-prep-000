65321b129fdfce841fce9c239c800804b22fca40
Alison Kanegae <alisonkanegae@gmail.com>
2018-09-02 02:29:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 //1
 function addToCart(item){
-  var price = Math.floor(Math.random() * 100);
+  var price = Math.floor(Math.random() * 100) + 1;
   cart.push({ [item]: price});
   console.log(`${item} has been added to your cart.`)
   return cart
