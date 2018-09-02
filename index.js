4f8411585905990e5fcad0e4b47b1cb6afecc7a5
Alison Kanegae <alisonkanegae@gmail.com>
2018-09-02 01:59:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,16 +19,17 @@ function addToCart(item) {
 
 //2
 function viewCart() {
-  if (cart.length === 0);
-  console.log("Your shopping cart is empty.");
-} else {
-  var items = [];
-  for (let i = 0; i < cart.length; i++) {
-    for(var item in cart[i]) {
-      items.push(item + " at $" + cart[i][item]);
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.")
+  } else {
+    var items = [];
+    for (var i = 0; i < cart.length; i++) {
+      for (var item in cart[i]) {
+        items.push(item + " at $" + cart[i][item])
+      }
     }
+    console.log("In your cart, you have " + items.join(", ") + ".");
   }
-  console.log("In your cart, you have" + items.join(", ") + ".");
 }
 
 //3
